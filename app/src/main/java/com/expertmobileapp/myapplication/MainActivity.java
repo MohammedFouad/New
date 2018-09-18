a3d49d2f42bd30bee9ae8a7350c16fe61f925d47
Mohamed <mohfoad@gmail.com>
2018-09-18 05:58:20 +0200

    Initial commit

---
+++ app/src/main/java/com/expertmobileapp/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.expertmobileapp.myapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.buttonCall);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:01019340026"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) !=  PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+
+                startActivity(callIntent);
+            }
+        });
+    }
+}
